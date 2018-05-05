77e5e182c15386e1a57ebdc4132f7fe591458cc3
edmobe <e.d.mobe@hotmail.com>
2018-05-05 09:59:00 -0600

    Create C# project

---
+++ Oddysey/Oddysey/Oddysey/Mainclass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oddysey
+{
+    class Mainclass
+    {
+        public static void Main(String[] args) {
+            Console.Out.WriteLine("Hola mundo");
+            Console.ReadKey();
+        }
+    }
+}
