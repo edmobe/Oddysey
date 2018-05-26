1719b8dee68d90a70c0aeb20f95eb87e3f10f44d
Bannersx <38147810+Bannersx@users.noreply.github.com>
2018-05-26 16:26:59 -0600

    Added the optimized login

--- Oddysey/UI/OddyseyUI/OddyseyUI/Form3.cs
+++ Oddysey/UI/OddyseyUI/OddyseyUI/Form3.cs
@@ -25,7 +25,9 @@ namespace OddyseyUI
         {
             String user = LogUsername.Text;
             String passw = LogPassword.Text;
-            //Validation process with the given data.
+            //Validation process with the given data. Checks if the spaces are filled and if the user exists.
+
+
             if (string.IsNullOrEmpty(user) && string.IsNullOrEmpty(passw))
             {
                 var result = MessageBox.Show("Please enter your username\r\nPlease enter your password");// message to ask to fill in the blanks
@@ -34,11 +36,14 @@ namespace OddyseyUI
             {
                 var result = MessageBox.Show("Please Fill in the blanks");
             }
+
+
             //Once Validated||
             else
             {
-                //form1.ChangeLabel(user); // volver a hacer por fis :(
+                form1.ChangeLabel(user);
                 this.Close();
+                form1.Logd = true;
             }
         }
 
@@ -49,8 +54,36 @@ namespace OddyseyUI
             String username = RegUsername.Text;
             String password = RePassword.Text;
             String favorites = RegFavGen.Text;
+            String age = textBox1.Text;
             String friends = RegFriends.Text;
+            String ans = "Please fill in the following spaces: ";
+            int auxi = 0;
+            //Auxiliary arrays to check which Spaces are empty
+            String[] form = { name, lastname, username, password, favorites, age, friends, age };
+            String[] aux = { "Name", "Last Name", "Username", "Password", "Favorite Genres", "Friends", "Age", "" };
+
+
             //Validation process of the username(can not already exist)
+            for (int i = 0; i < form.Length; i++)
+            {
+                if (string.IsNullOrEmpty(form[i]))
+                {
+                    //var result = MessageBox.Show("Please Fill in the " + aux[i] + " space");
+                    if (i < 6)
+                    {
+                        ans += " " + aux[i] + ",";
+                        auxi += 1;
+                    }
+                    else if (i == 6)
+                    {
+                        ans += " " + aux[i] + ".";
+                    }
+                }
+            }
+            if (auxi > 0)
+            {
+                var result = MessageBox.Show(ans);
+            }
 
 
             //Complete the registration.
